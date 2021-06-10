6300ee4b92717ede7d06b9420cb2f531f2f372a1
iswangyi <415268015@qq.com>
2021-06-11 00:48:54 +0800

    06011-1 commit

--- zinx/ziface/iconnection.go
+++ zinx/ziface/iconnection.go
@@ -1,1 +1,29 @@
 package ziface
+
+import "net"
+
+// IConnection 定义连接模块的抽象层
+type IConnection interface {
+	// Start 启动链接
+	Start()
+	// Stop 停止连接，结束当前的链接工作
+
+	Stop()
+
+	// GetTCPConnection 获取当前链接的scoket
+	GetTCPConnection() *net.TCPConn
+
+	// GetConnectionID 获取当前的连接ID
+	GetConnectionID() uint32
+
+	// GetRemoteAddr 获取远程客户端的TCP状态
+	GetRemoteAddr() net.Addr
+	// Send 发送数据
+	Send(data []byte)  error
+}
+
+
+
+//定义一个处理链接的方法
+
+type HandlerFunc func(*net.TCPConn,[]byte,int) error
--- zinx/znet/connection.go
+++ zinx/znet/connection.go
@@ -1,1 +1,52 @@
 package znet
+
+import (
+	"net"
+	"zjxzinx/zinx/ziface"
+)
+
+type Connection struct {
+	Conn *net.TCPConn
+	ConnID uint32
+	isClosed bool
+	HandlerFunc ziface.HandlerFunc
+	//告知当前链接状态的chan
+	ExitChan chan bool
+}
+
+
+
+func NewConnection(conn *net.Conn,connID uint32, callbackApi ziface.HandlerFunc )  *Connection{
+	c := &Connection{
+		Conn:        conn,
+		ConnID:      connID,
+		HandlerFunc: callbackApi,
+		isClosed:    false,
+		ExitChan:    make(chan bool,1),
+
+	}
+	return c
+}
+
+func (c *Connection)Start(){
+
+}
+
+func (c *Connection)Stop()  {
+
+}
+func (c *Connection)GetTCPConnection() *net.TCPConn {
+	return c.Conn
+
+}
+
+func (c *Connection)GetConnectionID() uint32  {
+	return c.ConnID
+}
+func (c *Connection)GetRemoteAddr() net.TCPConn   {
+	c.Conn.RemoteAddr()
+
+}
+func (c *Connection)Send() error {
+	return nil
+}
